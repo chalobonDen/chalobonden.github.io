8e2b93b813d15b9bea8af4e39e32478e22ba0506
chalobon <chalobon-jaje@hotmail.com>
2021-05-20 13:07:22 +0700

    fix profile image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,26 @@
 import * as React from 'react';
 import Layout from '../components/Layout';
 import ReactTypingEffect from 'react-typing-effect';
-import { StaticImage } from 'gatsby-plugin-image';
+// import { StaticImage } from 'gatsby-plugin-image';
 import Social from '../components/Social';
-
+import profile1 from '../images/profile1.jpg';
 const IndexPage = () => {
   return (
     <Layout>
       <section className="text-center m-5 flex justify-center items-center">
         <title>Home Page</title>
         <div className="block mb-30">
-          <StaticImage
+          {/* <StaticImage
             src="../images/profile1.jpg"
             alt="A profile"
             placeholder="blurred"
-            className="block mx-auto w-60 h-60  mb-10 mt-10"
+            className="block mx-auto w-60 h-60 rounded-full mb-10 mt-10"
             style={{ borderRadius: '100%' }}
+          /> */}
+          <img
+            src={profile1}
+            alt="profile"
+            className="block mx-auto w-60 h-60 rounded-full mb-10 mt-10"
           />
           <ReactTypingEffect
             text={['I am Chalobon', 'I am a Front-End Developer']}
