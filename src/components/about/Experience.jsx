3a967b7a583f0a59ebd1ba1d1b2b16cbe1380a0c
chalobon <chalobon-jaje@hotmail.com>
2021-07-11 16:16:02 +0700

    Revert "Revert "add API link""
    This reverts commit f75e584c903ad2ad2bf3512c09aebbd43dd80ee9.

--- src/components/about/Experience.jsx
+++ src/components/about/Experience.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Experience = ({ experienceData }) => {
+const Experience = ({ experienceData, getMonthToString }) => {
+  if (!experienceData) {
+    return <div></div>;
+  }
   return (
     <div>
       <div className="shadow-md py-4 px-8 bg-white rounded-md bg-opacity-30">
@@ -9,17 +12,18 @@ const Experience = ({ experienceData }) => {
           <h2 className="text-gray-700 text-2xl font-semibold">Experience</h2>
           <p className="font-bold mt-2 text-lg underline ">Internship</p>
           <div className=" lg:grid lg:grid-cols-2 lg:divide-x 2lg:grid-cols-1 2lg:divide-y">
-            {experienceData.map((data) => {
+            {experienceData.map((data, i) => {
               return (
                 <div
-                  key={data.id}
-                  className={`${
-                    data.id === 1 ? '2lg:pb-2' : 'lg:pl-6 2lg:pt-2'
-                  }`}
+                  key={i}
+                  className={`${i === 0 ? '2lg:pb-2' : 'lg:pl-6 2lg:pt-2'}`}
                 >
-                  <p className="font-bold">{data.jop_position}</p>
-                  <p>{data.period}</p>
-                  <p>{data.orgName}</p>
+                  <p className="font-bold">{data.position}</p>
+                  <p>
+                    {getMonthToString(data.start.month)} {data.start.year} -{' '}
+                    {getMonthToString(data.end.month)} {data.end.year}
+                  </p>
+                  <p>{data.company}</p>
                 </div>
               );
             })}
